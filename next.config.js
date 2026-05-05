dd0e9d3e5c6d00c683d44220c5589dd5ab1b0756
Viswanath Priyatham Javvaji <jvissup@users.noreply.github.com>
2026-05-04 23:31:45 -0700

    Remove Next.js configuration for image patterns

--- next.config.js
+++ next.config.js
@@ -1,14 +1,1 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.pokemontcg.io",
-      },
-    ],
-  },
-};
-
-export default nextConfig;
