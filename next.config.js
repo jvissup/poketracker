0e9e8cdd0d821b1e41b28557588701874ac4c127
Viswanath Priyatham Javvaji <jvissup@users.noreply.github.com>
2026-05-04 23:35:05 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "images.pokemontcg.io" }],
+  },
+};
 
+module.exports = nextConfig;
